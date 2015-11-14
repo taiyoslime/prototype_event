25729418984bf4578ae167eb8b7287754b2554c6
hase,tekitou@gmail.com <hase,tekitou@gmail.com>
2015-11-15 02:45:20 +0900

    today\'s last commit

--- js/main.js
+++ js/main.js
@@ -2,7 +2,13 @@ $( document ).ready(function(){
       $(document).ready(function(){
     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
     $('.modal-trigger').leanModal();
+          $('select').material_select();
   });
+    
+ $('.datetimepicker').datetimepicker({
+  format: 'Y-m-d H:i',
+  lang: 'ja'
+});
      
     $('#calendar').fullCalendar({
         
